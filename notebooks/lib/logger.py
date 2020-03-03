0705843b6c259720f4ce60f2591554b730b3f4c5
antalszava <antalszava@gmail.com>
2020-03-02 22:14:52 -0500

    Refactoring the logger

--- notebooks/lib/logger.py
+++ notebooks/lib/logger.py
@@ -4,52 +4,104 @@ import time
 import os
 import routing_simulation
 
-def log_graph_results(log: logging.Logger, graph_results: list, simulation_measures: list):
+
+simulation_measures = ['Average waiting times:', 'Number of available links:',
+                      'Number of available edges:', 'Average distances:']
+
+directory = 'logs'
+
+log = logging.getLogger()  # root logger
+
+def write_results_to_file(results: dict, elapsed_time: int):
+
+    initialize_logging()
+
+    create_logs_directory()
+    for algorithm in results.keys():
+        algorithm_results(results[algorithm], algorithm, '', elapsed_time)
+
+def algorithm_results(algorithm_results: list, algorithm: str, approach: str, elapsed_time: int):
+    timestr = time.strftime("%y_%m_%d__%H_%M")
+
+    details_in_name = [algorithm, str(routing_simulation.SimulationSettings().number_of_samples),
+                       approach, timestr, '.log']
+    filename = '_'.join(details_in_name)
+    filepath = os.path.join(directory, filename)
+
+    manage_handlers(filepath)
+
+    algorithm_and_approach_info(algorithm, approach)
+
+    dump_raw_results(algorithm_results)
+    dump_elapsed_time(elapsed_time)
+
+def manage_handlers(filepath):
+    """Removes old handlers and adds a new one.
+
+    Args:
+        fileh (logging.FileHandler): the filehandler to be used
+    """
+    fileh = logging.FileHandler(filepath, 'a')
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fileh.setFormatter(formatter)
+
+    for hdlr in log.handlers[:]:  # remove all old handlers
+        log.removeHandler(hdlr)
+    log.addHandler(fileh)  # set the new handler
+
+def initialize_logging():
+    """Initialize the logging by reloading and setting the basic configuration.
+    """
+    importlib.reload(logging)
+    logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s', level=logging.DEBUG, datefmt='%I:%M:%S')
+
+
+def algorithm_and_approach_info(algorithm_name, approach):
+    standard_message = 'Logging the simulation results of the ' + algorithm_name + ' algorithm'
+
+    # Names of simulation measures
+    if '' == approach:
+        log.debug(standard_message + '.')
+    else:
+        log.debug(standard_message + '(using the ' + approach + '.')
+    log.debug('The simulation measures are as follows: ')
+    log.debug(simulation_measures)
+
+def create_logs_directory():
+    """Create the logs directory, if it is non-existant yet.
+    """
+    os.makedirs(directory, exist_ok=True)
+
+def dump_raw_results(algorithm_results: list):
+    """Log all the results of the simulation.
+
+    Args:
+        simulation_results (list): the simulations results to process
+    """
+    log.debug('Detailed simulation results based on the graph measured: ')
+    for graph_index in range(len(algorithm_results)):
+        log.debug('graph' + str(graph_index) + ':')
+
+        # Log the stores containing the results
+        dump_graph_results(algorithm_results[graph_index])
+
+def dump_graph_results(graph_results: list):
+    """Log the results a specific graph in the simulation.
+
+    Args:
+        graph_results (list): the graph results to process
+    """
     for store_index in range(len(graph_results)):
         log.debug('-------------------------------------------------------------')
         log.debug(simulation_measures[store_index])
         log.debug('-------------------------------------------------------------')
         log.debug(graph_results[store_index])
 
+def dump_elapsed_time(elapsed_time):
+    """Log the results the elapsed time.
 
-def write_results_to_file(simulation_results: list, algorithm_name: str, approach: str, elapsed_time: int):
-    importlib.reload(logging)
-    logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s', level=logging.DEBUG, datefmt='%I:%M:%S')
-    timestr = time.strftime("%y_%m_%d__%H_%M")
-
-    # Create the logs directory, if it is non-existant yet
-    directory = 'logs'
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-
-    fileh = logging.FileHandler('./' + directory + '/' + algorithm_name + '_' +
-                                str(routing_simulation.Settings().number_of_samples) +
-                                '_' + approach + '_' + timestr + '.log', 'a')
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    fileh.setFormatter(formatter)
-
-    log = logging.getLogger()  # root logger
-    for hdlr in log.handlers[:]:  # remove all old handlers
-        log.removeHandler(hdlr)
-    log.addHandler(fileh)  # set the new handler
-
-    # Names of simulation measures
-    simulation_measures = ['Average waiting times:', 'Number of available links:',
-                          'Number of available edges:', 'Average distances:']
-
-    if '' == approach:
-        log.debug('Logging the simulation results of the ' + algorithm_name + ' algorithm.')
-    else:
-        log.debug(
-            'Logging the simulation results of the ' + algorithm_name + ' algorithm (using the ' + approach + '.')
-    log.debug('The simulation measures are as follows: ')
-    log.debug(simulation_measures)
-    log.debug('Detailed simulation results based on the graph measured: ')
-    for graph_index in range(len(simulation_results)):
-        log.debug('graph' + str(graph_index) + ':')
-
-        # Log the stores containing the results
-        log_graph_results(log, simulation_results[graph_index], simulation_measures)
-
-    # Log the elapsed time
+    Args:
+        elapsed_time (int): the time that has elapsed since the start of the
+            simulation
+    """
     log.debug('The elapsed time was: ' + str(elapsed_time))
